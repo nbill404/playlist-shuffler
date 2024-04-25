5469cfdff81012f572438e02137fa6510852c365
Bill Nguyen <nbill404@gmail.com>
2024-04-25 16:13:42 +1000

    Updated playlist types

---
+++ app/types/playlist.ts
@@ -0,0 +1,46 @@
+import { Song } from "./song";
+
+type Element = (Playlist | Song);
+
+export class Playlist {
+    rank: number = 0; // Rank/Depth of playlist
+    id: number;
+    name: string
+    elements: Element[] = []
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name;
+    }
+
+    add(e: Element) {
+        if (typeof e === typeof Playlist) {
+            e.rank = this.rank + 1;
+        }
+
+        this.elements.push(e);
+    }
+
+    remove(id: number | string) {
+        // Playlist id is a number, song id is a string
+        
+        try {
+            let index = 0;
+
+            for (; index < this.elements.length; index) {
+                // Explicit comparison will filter out wrong type
+                if (id === this.elements[index].id) {
+                    break;
+                }
+            }
+
+            if (index == this.elements.length) {
+                throw Error("Element not found in list");
+            } else {
+                this.elements.splice(index, index);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
--- app/types/song.ts
+++ app/types/song.ts
@@ -1,20 +1,26 @@
+class Details {
+    title: string
+    artist: string
+    length: number
+    thumbnail: string
+
+    constructor(title: string, artist: string, length: number, thumbnail: string) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+        this.thumbnail = thumbnail;
+    }
+}
+
 export class Song {
     id: string;
     platform: string;
-    
-    
-    // Details should contain
-    // title: string
-    // artist: string
-    // length: number
-    // thumbnail: string
-    
-    details: Object;
+    details: Details;
 
-    constructor(id: string, platform: string, details: Object) {
+    constructor(id: string, platform: string, details: Details) {
         this.id = id;
         this.platform = platform;
         this.details = details;
-
     }
 }
+
