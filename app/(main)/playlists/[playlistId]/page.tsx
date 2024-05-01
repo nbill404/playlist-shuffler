0ca112520f2369524a5e97977f580db8bb0ec604
Bill Nguyen <nbill404@gmail.com>
2024-05-01 20:46:29 +1000

    Fetch url params on page load

--- app/(main)/playlists/[playlistId]/page.tsx
+++ app/(main)/playlists/[playlistId]/page.tsx
@@ -1,8 +1,47 @@
-import DisplayPlaylistsView from "@/app/components/displaySongs/DisplaySongsView";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import Link from "next/link";
+import SongsDisplayList from "@/app/components/displaySongs/SongsDisplayList";
+import { Song } from "@/app/types/song";
 
-export default function DisplayPlaylistsPage() {
 
-    return <DisplayPlaylistsView/>
+export default async function DisplayPlaylistsPage({params}:
+    {params: {playlistId : string}}
+) {
+    const session = await getServerSession(authOptions)
+    const userId = session?.user.id;
 
+    const getSongsList = async (playlistId: string) => {
+        const data = {
+            userId: userId,
+            playlistId: playlistId
 
+        }
+
+        return fetch(process.env.URL + "/api/song/get", {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                let songs = []
+
+                for (const item of data.data) {                   
+                    songs.push(item)
+                }
+                
+                return songs;
+            }).catch((error) => {
+                return [];
+            })
+    }
+
+    const songs = await getSongsList(params.playlistId);
+
+    return (
+        <div className="m-3 p-5 bg-sky-950 rounded-md flex-1">
+            <Link className="btn btn-primary" href={process.env.URL + "/playlists"}>Back</Link>
+            <SongsDisplayList userId={userId} songs={songs}/>
+        </div>
+    )
 }
--- app/components/displaySongs/SongsDisplayList.tsx
+++ app/components/displaySongs/SongsDisplayList.tsx
@@ -1,41 +1,12 @@
-'use client'
-import { useEffect, useState } from "react"
 import MusicListElement from "./MusicListElement"
 import { Song } from "@/app/types/song"
-import { useParams } from "next/navigation"
 
 interface Props {
-    userId: string | undefined
+    userId: number | undefined
+    songs: Song[]
 }
 
-export default function SongsDisplayList({userId}: Props) {
-    const [songs, setSongs] = useState([]);
-    const { playlistId } = useParams()
-
-    // Fetch song data
-    useEffect(() => {
-        const data = {
-            userId: userId,
-            playlistId: playlistId
-
-        }
-
-        fetch("/api/song/get", {
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                let songs = [];
-
-                for (const item of data.data) {                   
-                    songs.push(item)
-                }
-
-                setSongs(songs)
-            })
-    }, [userId, playlistId]);
-
+export default function SongsDisplayList({userId, songs}: Props) {
     return (
         <div>
             {songs && songs.map((song : Song, index: number) =>
