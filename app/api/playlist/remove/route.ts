71021edfbcbc0e23d2efe18c0a8ff153746dae97
Bill Nguyen <nbill404@gmail.com>
2024-05-08 15:55:53 +1000

    Removing a playlist deletes all associated songs from database

--- app/api/playlist/remove/route.ts
+++ app/api/playlist/remove/route.ts
@@ -1,10 +1,20 @@
 import { db } from "@/app/lib/db";
+import { Whisper } from "next/font/google";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     try {
         // user id, playlist id
         const {userId, playlist} = await req.json();
+
+        // Delete all associate songs
+        await db.song.deleteMany({
+            where: {
+                userId: Number(userId),
+                playlistId: playlist.id
+            }
+        }
+        );
 
         // Change to delete
         await db.playlist.delete({
