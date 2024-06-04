ecff15f32322112efb0d4844204194301c026b90
Bill Nguyen <nbill404@gmail.com>
2024-06-04 22:00:19 +1000

    uses client

--- app/components/playlist/RankButton.tsx
+++ app/components/playlist/RankButton.tsx
@@ -1,9 +1,9 @@
+'use client'
 import Link from "next/link";
-
 
 export default function RankButton({rank} : {rank : number}) {
     return (
-        <Link className="btn btn-neutral flex items-center justify-center" href={`${process.env.URL}/playlists?r=${rank}`}>
+        <Link className="btn btn-neutral flex items-center justify-center" href={`/playlists?r=${rank}`}>
             <p className="text-xl w-4 h-8">{rank + 1}</p>
         </Link>
     )
