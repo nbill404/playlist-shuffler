523d4ad1889f3f763eb82081b67d3aadbfa5461b
Bill Nguyen <nbill404@gmail.com>
2024-06-07 22:23:19 +1000

    Added max duration

--- app/api/playlist/getSub/route.ts
+++ app/api/playlist/getSub/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 60; // Vercel max duration of api call
+
 // Retrieves get details of sub playlists given parent
 export async function POST(req: Request) {
     try {
